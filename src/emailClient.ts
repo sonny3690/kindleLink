f7bcfbd343e2ea3abb66358e96ad1766011d8e95
Sonny Mo <sonny3690@gmail.com>
2020-06-05 01:09:53 -0400

    feat: complete email client

--- src/emailClient.ts
+++ src/emailClient.ts
@@ -1,40 +1,39 @@
 import mailer from 'nodemailer'
 import fs from 'fs'
-
+import dotenv from 'dotenv'
 const email = 'kindle.email.service@gmail.com'
 const recipient = 'sonny3690@gmail.com'
+const downloadDir = './download'
+const books = fs.readdirSync('./download')
 
+dotenv.config()
 
 const transporter = mailer.createTransport({
   service: 'gmail',
-
   auth: {
     user: email,
     pass: process.env.EMAIL_PW
   }
 })
 
+const attachments = books.map(
+  book => ({ filename: book, path: `${downloadDir}/${book}` })
+)
+
 const mailOptions = {
   from: email,
   to: recipient,
   subject: 'Your Kindle Book!',
+  attachments
 }
 
-
-transporter.sendMailer(mailOptions, function (error: any, info: any) {
+transporter.sendMail(mailOptions, function (error: any, info: any) {
   if (error) {
     console.error('Error in sending message: ' + error)
   } else {
-    console.log('Email sent! ' + info)
+    console.log('Email sent! ' + JSON.stringify(info, null, '\t'))
   }
 })
 
-fs.readdirSync('./download').forEach(
-
-  file => {
-    console.log(file)
-  }
-
-)
 
 
