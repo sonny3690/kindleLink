c73bdcc16fe5979611fd2de287b22662172cee12
Sonny Mo <sonny3690@gmail.com>
2020-06-05 01:23:59 -0400

    chore: setup view

---
+++ src/view.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const Stuff = () => {
+
+  return 'hello world'
+}
+
+
+
+render(<Stuff />, document.getElementById('root'))
+
